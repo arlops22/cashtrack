431e870c8517cae8bfc39e71404b5d40ca9dfb14
arlops22 <ary.s.lopes22@gmail.com>
2026-09-12 15:05:28 -0300

    feat: setup API healthcheck

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Response } from 'express';
 import { pinoHttp } from 'pino-http';
 import { logger } from './config/logger.config';
 
@@ -7,12 +7,21 @@ export class App {
 
     constructor() {
         this.app = express();
+
+        this.initMiddlewares();
+        this.initializeRoutes();
     }
 
-    initMiddlewares() {
+    private initMiddlewares() {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(pinoHttp({ logger }));
+    }
+
+    private initializeRoutes() {
+        this.app.get('/health-check', (_, res: Response) => {
+            res.status(200).json({ message: 'API running well!' });
+        });
     }
 
     listen(port: string): void {
